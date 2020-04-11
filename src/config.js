dbc1af557e8c4dbb877048d185334b78fb922151
Ian <ianfreitas1@poli.ufrj.br>
2020-04-11 15:26:13 -0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "sa-east-1",
+    BUCKET: "notes-ian"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://6ukt3i838h.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2"
+    USER_POOL_ID: "us-east-2_vC0wOSxG4",
+    APP_CLIENT_ID: "1jlkf3ddd4s4deiutd472qshi",
+    IDENTITY_POOL_ID: "us-east-2:8293dcbb-89ea-4213-a5c2-5862633e16bd"
+  }
+};
